a0c9dc4aee3fc9c4ccd1170b18f9e26a51a65471
Yvo-spks <yimongan@yahoo.com>
2022-12-19 13:37:59 +0100

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+     <h1>
       Weather App
+      </h1>
+      <footer>
+        This project was coded by Yvonne Imongan and is open-sourced {" "}
+      <a href="https://github.com/Yvo-spks/react-weather-app" target="_blank" rel="noreferrer">
+
+        open-sourced on GitHub
+
+      </a>
+      </footer>
     </div>
   );
 }
